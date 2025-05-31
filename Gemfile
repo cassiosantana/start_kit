ea95ddcce67c5c3e20fe43771a05af989be8dbd1
Cassio Santana <cassioo.roger@gmail.com>
2025-05-30 21:08:13 -0300

    feat: add start kit

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 gem "rails", "~> 7.2.2", ">= 7.2.2.1"
 
 gem "bootsnap", require: false
+gem "devise"
 gem "jbuilder"
 gem "jsbundling-rails"
 gem "pg", "~> 1.1"
@@ -11,10 +12,12 @@ gem "sprockets-rails"
 gem "stimulus-rails"
 gem "turbo-rails"
 gem "tzinfo-data", platforms: %i[ windows jruby ]
+gem "ffaker"
 
 group :development, :test do
   gem "brakeman", require: false
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
+  gem "rspec-rails"
   gem "rubocop-rails-omakase", require: false
 end
 
@@ -23,6 +26,7 @@ group :development do
 end
 
 group :test do
-gem "capybara"
+  gem "capybara"
   gem "selenium-webdriver"
+  gem "shoulda-matchers"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Book.destroy_all
+
+puts "Starting Book Creation..."
+
+5.times do |i|
+  Book.create!(
+    title: FFaker::Book.title,
+    author: FFaker::Name.name
+  )
+  puts "Book #{i+1} successfully created!"
+end
+
+puts "Were created #{Book.count} Books."
